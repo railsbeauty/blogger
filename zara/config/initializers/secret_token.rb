4b8d58ac0aca1a676978727f3144339403b32c32
vinay <vinay@vertisinfotech.com>
2013-02-12 14:18:46 +0530

    initial commit

---
+++ zara/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zara::Application.config.secret_token = '0442546455cfb69a46e7594512607165f092f43dd28bf17b255683fabdeab34b7cc93b6a8114828eba3b6ef3018f9d63e13d751d81a3be1ad041b3a1d551be23'
